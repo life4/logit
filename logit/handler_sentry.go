5ce71bb9322c0a7d71a233e25c7f2fafa77ada9e
gram <mail@orsinium.dev>
2020-12-30 14:15:54 +0100

    sentry timeout

--- logit/handler_sentry.go
+++ logit/handler_sentry.go
@@ -1,13 +1,17 @@
 package logit
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/evalphobia/logrus_sentry"
 	"github.com/sirupsen/logrus"
 )
 
 type SentryHandler struct {
 	BaseHandler
-	DSN string
+	DSN     string
+	Timeout string
 }
 
 func NewSentryHandler() SentryHandler {
@@ -29,6 +33,11 @@ func (config SentryHandler) Parse() (*Handler, error) {
 	hook, err := logrus_sentry.NewSentryHook(config.DSN, allLevels)
 	if err != nil {
 		return nil, err
+	}
+
+	hook.Timeout, err = time.ParseDuration(config.Timeout)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse timeout: %v", err)
 	}
 
 	h, err := config.BaseHandler.Parse()
