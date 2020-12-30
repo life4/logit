0e0b87ac00c19211331933592f64bfc2b184cbc6
gram <mail@orsinium.dev>
2020-12-30 13:51:18 +0100

    support writing into a file

--- logit/handler_base.go
+++ logit/handler_base.go
@@ -1,7 +1,10 @@
 package logit
 
 import (
+	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 )
@@ -9,12 +12,15 @@ import (
 type BaseHandler struct {
 	LevelFrom string `toml:"level_from"`
 	LevelTo   string `toml:"level_to"`
+	File      string
+	Mode      os.FileMode
 }
 
 func NewBaseHandler() BaseHandler {
 	return BaseHandler{
 		LevelFrom: "trace",
 		LevelTo:   "panic",
+		File:      "stdout",
 	}
 }
 
@@ -27,8 +33,26 @@ func (config BaseHandler) Parse() (*Handler, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	var stream io.Writer
+	switch strings.ToLower(config.File) {
+	case "stdout":
+		stream = os.Stdout
+	case "stderr":
+		stream = os.Stderr
+	default:
+		stream, err = os.OpenFile(
+			config.File,
+			os.O_WRONLY|os.O_APPEND|os.O_CREATE,
+			config.Mode,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("cannot open file: %v", err)
+		}
+	}
+
 	h := Handler{
-		stream:    os.Stdout,
+		stream:    stream,
 		levelFrom: lfrom,
 		levelTo:   lto,
 	}
