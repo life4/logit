4a369f97a0b76bbc0384f185cc31721fdffdd1b8
gram <mail@orsinium.dev>
2020-12-30 11:30:53 +0100

    handlers

--- logit/config.go
+++ logit/config.go
@@ -21,14 +21,14 @@ type CMain struct {
 // 	Trace   string
 // }
 
-type CFormatters struct {
-	Text FText
-	JSON FJSON
+type CHandler struct {
+	Formatter string
 }
 
 type Config struct {
-	Main       CMain
-	Formatters CFormatters
+	Main        CMain
+	HandlersRaw []toml.Primitive `toml:"handler"`
+	formatters  []logrus.Formatter
 }
 
 func NewConfig() Config {
@@ -36,10 +36,6 @@ func NewConfig() Config {
 		Main: CMain{
 			Level:     "trace",
 			Formatter: "text",
-		},
-		Formatters: CFormatters{
-			Text: NewFText(),
-			JSON: NewFJSON(),
 		},
 	}
 	return c
@@ -55,22 +51,53 @@ func ReadConfig(cpath string) (*Config, error) {
 	if err != nil {
 		return nil, fmt.Errorf("cannot read config: %v", err)
 	}
-	undecoded := meta.Undecoded()
-	if len(undecoded) != 0 {
-		return nil, fmt.Errorf("unknown fields: %v", undecoded)
+
+	config.formatters = make([]logrus.Formatter, len(config.HandlersRaw))
+	for i, primitive := range config.HandlersRaw {
+		f, err := parseFormatter(meta, primitive)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse handler: %v", err)
+		}
+		config.formatters[i] = f
 	}
+
+	// fmt.Println(config.Handlers[0])
+	// undecoded := meta.Undecoded()
+	// if len(undecoded) != 0 {
+	// 	return nil, fmt.Errorf("unknown fields: %v", undecoded)
+	// }
 	return &config, nil
 }
 
-func (c *Config) Formatter() (logrus.Formatter, error) {
-	switch c.Main.Formatter {
-	case "text":
-		return FTextParse(c.Formatters.Text)
-	case "json":
-		return FJSONParse(c.Formatters.JSON)
-	default:
-		return nil, fmt.Errorf("unknown formatter: %s", c.Main.Formatter)
+func parseFormatter(meta toml.MetaData, primitive toml.Primitive) (logrus.Formatter, error) {
+	var h CHandler
+	err := meta.PrimitiveDecode(primitive, &h)
+	if err != nil {
+		return nil, err
 	}
+
+	switch h.Formatter {
+	case "text":
+		fconf := NewFText()
+		err = meta.PrimitiveDecode(primitive, &fconf)
+		if err != nil {
+			return nil, err
+		}
+		return FTextParse(fconf)
+	case "json":
+		fconf := NewFJSON()
+		err = meta.PrimitiveDecode(primitive, &fconf)
+		if err != nil {
+			return nil, err
+		}
+		return FJSONParse(fconf)
+	default:
+		return nil, fmt.Errorf("unknown formatter: %s", h.Formatter)
+	}
+}
+
+func (c *Config) Formatter() (logrus.Formatter, error) {
+	return c.formatters[0], nil
 }
 
 func (c *Config) Level() (logrus.Level, error) {
